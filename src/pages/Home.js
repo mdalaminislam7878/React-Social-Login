fd244727dcf6b1d1e1ff5233502b6a7a525777f4
Md. Alamin <mdalaminislam7878@gmail.com>
2022-01-12 20:42:17 +0600

    data updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import { posts } from "../data";
 const Home = () => {
     return (  
         <div className="home">
-            <h1>Hello Deploy</h1>
             <section className="product">   
                 { posts.map((post) => (
                     <Card key={post.id} post= { post }/>
