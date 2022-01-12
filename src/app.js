4fe072579d23e5bb74542652c1b3ca00dbd24fb5
Md. Alamin <mdalaminislam7878@gmail.com>
2022-01-13 02:53:42 +0600

    add commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import { useEffect, useState } from 'react';
 const App = () => {
     const [user, setUser] = useState(null);
     useEffect(() => {
-        fetch(`https://mdalaminislam787-social.herokuapp.com/auth/login/success`, {
+        // fetch(`https://mdalaminislam787-social.herokuapp.com/auth/login/success`, {
+        fetch(`https://randomuser.me/api/`, {
             method: 'GET',
             credentials: 'include',
             headers: {
