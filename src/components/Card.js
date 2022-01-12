646b51beca1ee37757ecfd4ad4b13e74975aa27a
Md. Alamin <mdalaminislam7878@gmail.com>
2022-01-12 13:18:45 +0600

    data updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
 const Card = ({ post }) => {
-    const path = "React-Social-Login";
+    const path = "/React-Social-Login";
     return (  
         <div className="card">
-            <Link to={`${path}/post/${post.id}`}>
+            <Link to={`post/${post.id}`}>
                 <h3 className="card-title">{ post.title }</h3>
                 <img className="card-image" src={ post.img } alt="" />
                 <p className="card-desc">{ post.desc }</p>
